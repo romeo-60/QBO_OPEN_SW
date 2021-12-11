f44e32268d8527e9c7acf3aeda06cb3bae8c5d5a
romeo-60 <romeo.ceccato@gmail.com>
2021-12-11 13:04:02 +0100

    11_12_2021

--- QBO_TEST/TTS_01.py
+++ QBO_TEST/TTS_01.py
@@ -5,18 +5,18 @@ import subprocess
 
 
 txt = (
-"uno",
-"due"
+"test uno",
+"test due"
 )
 
 spk_01 = 'espeak -v mb-it4 -s 120 -p 35 -w /tmp/temp.wav' + ' " '+ txt[0]+ ' " '+ '&& aplay -r 8000 -D  convertQBO /tmp/temp.wav'
 print(spk_01)
 subprocess.call(spk_01, shell=True)
 print("-1-")
-spk_02 = 'espeak -v mb-it3 -s 120 -p 35 -w /tmp/temp.wav "numero due" && aplay -r 8000 -D  convertQBO /tmp/temp.wav'
+spk_02 = 'espeak -v mb-it3 -s 120 -p 35 -w /tmp/temp.wav "numero uno" && aplay -r 8000 -D  convertQBO /tmp/temp.wav'
 subprocess.call(spk_02, shell=True)
 print("-2-")
-spk_03 = 'espeak -v it -s 120 -p 35 -w /tmp/temp.wav "numero tre" && aplay -r 48000 -D  convertQBO /tmp/temp.wav'
+spk_03 = 'espeak -v it -s 120 -p 35 -w /tmp/temp.wav "numero due" && aplay -r 48000 -D  convertQBO /tmp/temp.wav'
 subprocess.call(spk_03, shell=True)
 print ("-3-")
 print("---------------------")
